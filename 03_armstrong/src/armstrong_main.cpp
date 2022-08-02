dfba0b9a767ebafab6ffa8a1975315cd90d0955c
Duca Robert <robert6gto@gmail.com>
2022-08-02 17:14:29 +0300

    modofied armstrong_main.cpp

--- 03_armstrong/src/armstrong_main.cpp
+++ 03_armstrong/src/armstrong_main.cpp
@@ -1,25 +1,61 @@
 #include <iostream>
 
+int power(int number, unsigned int y)
+{
+	if (y == 0)
+		return 1;
+	if (y % 2 == 0)
+		return power(number, y / 2) * power(number, y / 2);
+	return number * power(number, y / 2) * power(number, y / 2);
+}
+int order(int number)
+{
+	int n = 0;
+	while (number)
+	{
+		n++;
+		number = number / 10;
+	}
+	return n;
+}
+
 bool isArmstrongNumber(int number)
 {
+	int n = order(number);
+	int temp = number, sum = 0;
+
+
+	while (temp)
+	{
+
+		int r = temp % 10;
+		sum = sum + power(r, n);
+		temp = temp / 10;
+	}
 	// TODO: implement some functionality to see if this number is an armstrong number
 
-	return false;
+	return (sum == number);
 }
 
 void printIsArmstrong(int number)
 {
-	if (isArmstrongNumber(number))
-	{
-		std::cout << "Armstrong" << std::endl;
-	}
-	else
-	{
-		std::cout << "NOT Armstrong" << std::endl;
-	}
+		if (isArmstrongNumber(number))
+		{
+			std::cout << "Armstrong" << std::endl;
+		}
+		else
+		{
+			std::cout << "NOT Armstrong" << std::endl;
+		}
 }
 
-int main(int argc, char *argv[])
+void ArmstrongString(int number)
+{
+	
+
+}
+
+int main(int argc, char* argv[])
 {
 	// What is this program expected to do?
 	// - Shows whether an argument is an armstrong number.
@@ -50,9 +86,23 @@ int main(int argc, char *argv[])
 	int readNumber = 0;
 	// Get the first argument
 	std::string argumentAsString = argv[1];
+	const char* argumentAsCharArray = argumentAsString.c_str();
 	
-	// TODO: read number / cast to integer
+	readNumber = atoi(argumentAsCharArray);
 
-	printIsArmstrong(readNumber);
+	// TODO: read number / cast to integer
+for (int i = 0; i < strlen(argumentAsCharArray); i++)
+	{
+		if (isdigit(argumentAsCharArray[i]))
+		{
+			printIsArmstrong(readNumber);
+			break;
+		}
+		else 
+		{
+			std::cout << "it didin't take expected input" << std::endl;
+			break;
+		}
+	}
 	return 0;
 }
